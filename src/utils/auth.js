93ad479807e144d7292124ed2ef526b560ca2035
Readily <16585768+readily14@user.noreply.gitee.com>
2026-05-26 15:05:17 +0800

    test

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -5,3 +5,5 @@ export function setToken(token) { localStorage.setItem(TOKEN_KEY, token); }
 export function removeToken() { localStorage.removeItem(TOKEN_KEY); }
 
 export default { getToken, setToken, removeToken };
+
+
